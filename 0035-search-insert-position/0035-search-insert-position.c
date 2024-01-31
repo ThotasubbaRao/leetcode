ce473f7005c2f16ea346d1931adad4664697cb24
ThotasubbaRao <121806568+ThotasubbaRao@users.noreply.github.com>
2024-01-31 07:26:57 +0530

    Time: 5 ms (59.96%), Space: 6 MB (86.91%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,17 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int i=0;
+        int j = numsSize-1;
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]< target){
+                i = mid+1;
+            }
+            else{
+                j =mid-1;
+            }
+        }
+        return i;
+}
