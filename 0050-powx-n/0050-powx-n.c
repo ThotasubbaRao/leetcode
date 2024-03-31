e0a97be10639be9ab04dd6d98c268a33ddbc4cea
ThotasubbaRao <121806568+ThotasubbaRao@users.noreply.github.com>
2024-03-31 09:28:40 +0530

    Time: 4 ms (11.25%), Space: 5.7 MB (24.67%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.c
@@ -0,0 +1,19 @@
+double myPow(double x, int n) {
+    long N =n;
+    return pow(x,N);
+
+}
+double pow24(double x,long n){
+    if(n < 0){
+        n = -n;
+        x = 1/x;
+    }
+    if(n==0) return 1;
+    if(n==1) return x;
+    if(n%2==0){
+        return pow24(x *x , n/2);
+    }
+    else{
+        return x * pow24(x *x ,(n-1)/2);  
+    }
+}
