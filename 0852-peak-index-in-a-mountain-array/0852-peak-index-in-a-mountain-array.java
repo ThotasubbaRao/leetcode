61cb010c17cce84a3d60e3701cfe89693486cf23
ThotasubbaRao <121806568+ThotasubbaRao@users.noreply.github.com>
2024-06-10 09:49:09 +0530

    Time: 0 ms (100.00%), Space: 56.1 MB (50.63%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,24 +1,16 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        return binarySearch(arr);
-
-    }
-    static int binarySearch(int[] arr){
-        int start=0;
-        int end = arr.length-1;
-        while(start<=end){
-            int mid = (start+end)/2;
-            if(arr[mid]>arr[mid+1] && arr[mid] >arr[mid-1]){
-                 return mid;
-            }
-            else if(arr[mid] < arr[mid+1]){
+          int start =0;
+          int end = arr.length-1;
+          while(start < end){
+            int mid = start+ (end-start)/2;
+            if(arr[mid] < arr[mid+1]){
                 start = mid+1;
             }
-            else if(arr[mid] < arr[mid-1]){
-                end = mid-1;
+            else{
+                end = mid;
             }
-        }
-        return -1;
+          }
+          return start;
     }
-
 }
