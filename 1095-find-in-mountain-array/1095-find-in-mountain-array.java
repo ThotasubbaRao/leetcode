d4137cf2cb301ec4f4d802f1d8a07ca37935a04a
ThotasubbaRao <121806568+ThotasubbaRao@users.noreply.github.com>
2024-06-11 09:30:01 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (92.46%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.java
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -32,34 +32,30 @@ class Solution {
 		}
 		return start;
 	}
-    public static int binary( MountainArray arr,int start,int end,int target) {
-	    if(arr.get(start) < arr.get(end)) {
-	    	while(start<=end) {
-				int mid = start+(end-start)/2;
-				if(arr.get(mid) ==target) {
-					return mid;
-				}
-				else if(arr.get(mid) > target) {
-					end = mid-1;
-				}else {
-					start = mid+1;
-				}
-			}
-	    }
-	    else {
+    public static int binary(MountainArray arr,int start,int end,int target) {
+		boolean IsASC = arr.get(start) < arr.get(end);
+	    
 	    	while(start<=end) {
 				int mid = start+(end-start)/2;
 				if(arr.get(mid)==target) {
 					return mid;
 				}
-				else if(arr.get(mid) > target) {
-					start = mid+1;
-				}else {
-					end = mid-1;				
-                }
+				if(IsASC) {
+					if(arr.get(mid) > target) {
+						end = mid-1;
+					}else {
+						start = mid+1;
+					}
+				}
+				else {
+					if(arr.get(mid) < target) {
+						end = mid-1;
+					}else {
+						start = mid+1;
+					}
+				}
+				
 			}
-	    }
-		
 		return -1;
 		
 	}
