2855ca2676983405c9d60f938040ce58f37ff55c
ThotasubbaRao <121806568+ThotasubbaRao@users.noreply.github.com>
2024-06-11 09:15:15 +0530

    Time: 0 ms (100.00%), Space: 43 MB (56.39%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,66 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int peakindex =findelement(mountainArr);
+        int minindex = binary(mountainArr,0,peakindex,target);
+		if(minindex==-1) {
+		  return binary(mountainArr,peakindex+1,mountainArr.length()-1,target) ;
+		}else {
+			return minindex;
+	    }
+    }
+    public static int findelement(MountainArray arr) {
+		int start =0;
+		int end = arr.length()-1;
+		
+		while(start<end) {
+			int mid = start+ (end-start)/2;
+			if(arr.get(mid) > arr.get(mid+1)){
+				end = mid;
+			}
+			else {
+				start = mid+1;
+			}
+		}
+		return start;
+	}
+    public static int binary( MountainArray arr,int start,int end,int target) {
+	    if(arr.get(start) < arr.get(end)) {
+	    	while(start<=end) {
+				int mid = start+(end-start)/2;
+				if(arr.get(mid) ==target) {
+					return mid;
+				}
+				else if(arr.get(mid) > target) {
+					end = mid-1;
+				}else {
+					start = mid+1;
+				}
+			}
+	    }
+	    else {
+	    	while(start<=end) {
+				int mid = start+(end-start)/2;
+				if(arr.get(mid)==target) {
+					return mid;
+				}
+				else if(arr.get(mid) > target) {
+					start = mid+1;
+				}else {
+					end = mid-1;				
+                }
+			}
+	    }
+		
+		return -1;
+		
+	}
+}
